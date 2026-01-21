a3143f531e42e9f208cfd5eb6daada40e8c83125
rplotner10 <rplotner@carthage.edu>
2026-01-21 14:59:00 -0600

    Added simple read in file and print out file methods

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#inlcude <iostream>
+#include <iostream>
+#include "mdConverter.h"
+#include <string>
+#include <sstream>
+#include <fstream>
 
+using namespace std;
 int main() 
 {
 
+    ifstream inputFile("markdown-sample.md");
+
+    if (!inputFile.is_open()) 
+    {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    ofstream outputFile("output.txt");
 
 
     return 0;
